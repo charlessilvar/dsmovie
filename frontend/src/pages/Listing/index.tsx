958b9324faf426a01cece957a05c58d8ecdc94b1
charlessilvar <charlessilvar@gmail.com>
2022-05-21 23:35:56 -0300

    Pagination Layout

--- frontend/src/pages/Listing/index.tsx
+++ frontend/src/pages/Listing/index.tsx
@@ -1,6 +1,18 @@
+import { ReactComponent as Arrow } from 'assets/img/Previous.svg';
+
 function Listing(){
     return (
-        <h1>Listagem</h1>
+        <div className="dsmovie-pagination-container">
+    <div className="dsmovie-pagination-box">
+        <button className="dsmovie-pagination-button" disabled={true} >
+            <Arrow />
+        </button>
+        <p>{`${1} de ${3}`}</p>
+        <button className="dsmovie-pagination-button" disabled={false} >
+            <Arrow className="dsmovie-flip-horizontal" />
+        </button>
+    </div>
+</div>
     );
 }
 
